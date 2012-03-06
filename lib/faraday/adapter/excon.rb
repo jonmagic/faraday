7e8ca282950e8f613b830a7a5209d1a16befe5c7
Jonathan Hoyt <hoyt@github.com>
2012-03-06 13:56:59 -0800

    Update Excon usage to work with Excon 0.10.0

--- lib/faraday/adapter/excon.rb
+++ lib/faraday/adapter/excon.rb
@@ -8,8 +8,8 @@ module Faraday
 
         conn = ::Excon.new(env[:url].to_s)
         if ssl = (env[:url].scheme == 'https' && env[:ssl])
-          ::Excon.ssl_verify_peer = !!ssl.fetch(:verify, true)
-          ::Excon.ssl_ca_path = ssl[:ca_file] if ssl[:ca_file]
+          ::Excon.defaults['ssl_verify_peer'] = !!ssl.fetch(:verify, true)
+          ::Excon.defaults['ssl_ca_path'] = ssl[:ca_file] if ssl[:ca_file]
         end
 
         resp = conn.request \
